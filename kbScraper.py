acab28db66ff2c73d44a85e5d089cfb12994178c
ItsTheRealVictor <74051392+ItsTheRealVictor@users.noreply.github.com>
2021-09-24 11:51:40 -0700

    Update kbScraper.py
    Added a while loop to iterate through product pages to pull data from all product listings, not just those on the first page

--- kbScraper.py
+++ kbScraper.py
@@ -1,19 +1,22 @@
 from bs4 import BeautifulSoup as bs
 import requests, lxml
 
-URL = "https://kbdfans.com/collections/keycaps?page=1"
+URL = "https://kbdfans.com/collections/keycaps"
 page = requests.get(URL)
 soup = bs(page.content, "lxml")
 
+priceList = []
+productTitleList = []
 
-#lists containing product data
-productPrices = soup.find_all("span", {"class":"theme-money"})
-productTitles = soup.find_all("div", {"class":"product-block__title"})
+pageIndex = 1
+while pageIndex != 19 + 1:
+    pageURL = URL + f"?page={pageIndex}"
+    pageURL_requests = requests.get(pageURL)
+    pageSoup = bs(pageURL_requests.content, 'lxml')
+    for price in pageSoup.find_all("span", {"class": "theme-money"}):
+        priceList.append(price.text)
+    pageIndex += 1
 
+print((priceList[5:10])) # a test to see if the while loop works
 
-
-#Test loop to see if find_all() function retrieved desired data
-for productTitle, price in zip(productTitles, productPrices):
-    price = str(price.text)
-    productTitle = str(productTitle.text)
-    print(productTitle + price)
+print('end')
