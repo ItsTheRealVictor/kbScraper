e4fbf9f9ec53e0b5afebd149046a49fccad5ae4b
ItsTheRealVictor <74051392+ItsTheRealVictor@users.noreply.github.com>
2021-09-24 10:54:44 -0700

    Update kbScraper.py
    now able to grab product titles and their prices, putting the data into respective lists.

--- kbScraper.py
+++ kbScraper.py
@@ -1,6 +1,19 @@
 from bs4 import BeautifulSoup as bs
 import requests, lxml
 
-URL = "https://realpython.com/beautiful-soup-web-scraper-python/"
+URL = "https://kbdfans.com/collections/keycaps?page=1"
 page = requests.get(URL)
 soup = bs(page.content, "lxml")
+
+
+#lists containing product data
+productPrices = soup.find_all("span", {"class":"theme-money"})
+productTitles = soup.find_all("div", {"class":"product-block__title"})
+
+
+
+#Test loop to see if find_all() function retrieved desired data
+for productTitle, price in zip(productTitles, productPrices):
+    price = str(price.text)
+    productTitle = str(productTitle.text)
+    print(productTitle + price)
