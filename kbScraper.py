70ed377ea0acf5fc7048f2c079eae6005bf31141
ItsTheRealVictor <74051392+ItsTheRealVictor@users.noreply.github.com>
2021-09-25 12:55:53 -0700

    Update kbScraper.py
    Added the scraper class to handle the data fetching.

--- kbScraper.py
+++ kbScraper.py
@@ -1,22 +1,46 @@
 from bs4 import BeautifulSoup as bs
-import requests, lxml
+import requests
+import lxml
 
-URL = "https://kbdfans.com/collections/keycaps"
-page = requests.get(URL)
-soup = bs(page.content, "lxml")
 
-priceList = []
-productTitleList = []
+class Scraper:
 
-pageIndex = 1
-while pageIndex != 19 + 1:
-    pageURL = URL + f"?page={pageIndex}"
-    pageURL_requests = requests.get(pageURL)
-    pageSoup = bs(pageURL_requests.content, 'lxml')
-    for price in pageSoup.find_all("span", {"class": "theme-money"}):
-        priceList.append(price.text)
-    pageIndex += 1
+    def __init__(self, URL, page, soup):
+        self.URL = URL
+        self.page = page
+        self.soup = soup
 
-print((priceList[5:10])) # a test to see if the while loop works
+
+    URL = "https://kbdfans.com/collections/keycaps"
+    page = requests.get(URL)
+    soup = bs(page.content, "lxml")
+
+    priceList = []
+    productTitleList = []
+
+
+    def getData():
+
+        pageIndex = 1
+        while pageIndex != 19 + 1:
+            pageURL = Scraper.URL + f"?page={pageIndex}"
+            pageURL_requests = requests.get(pageURL)
+            pageSoup = bs(pageURL_requests.content, 'lxml')
+            for price in pageSoup.find_all("span", {"class": "theme-money"}):
+                Scraper.priceList.append(price.text)
+            for product in pageSoup.find_all("div", {"class": "product-block__title"}):
+                Scraper.productTitleList.append(product.text)
+            pageIndex += 1
+
+
+    def printData():
+
+        for product, price in zip(Scraper.productTitleList, Scraper.priceList):
+            print(product.strip() + "..." + price.strip())
+
+
+
+Scraper.getData()
+Scraper.printData()
 
 print('end')
